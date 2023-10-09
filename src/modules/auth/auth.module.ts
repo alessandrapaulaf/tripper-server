66a1ea1f5b33959636feca8b9620d46d6a8f4487
alessandrapaulaf <alessandra.paulafrancisco@gmail.com>
2023-10-09 19:28:48 -0300

    (chore) add auth module

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
+
+@Module({
+  imports: [],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
