57b9e0c3e3f97e3b884c02d2e06b5f93b4e1ca2f
alessandrapaulaf <alessandra.paulafrancisco@gmail.com>
2023-10-11 18:53:03 -0300

    (chore) connect mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeorm.config';
 import { DataSource } from 'typeorm';
 import { AuthModule } from './modules/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppConfig } from './config/constants.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    MongooseModule.forRoot(AppConfig.mongoUrl, { dbName: AppConfig.appName }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/constants.config.ts
+++ src/config/constants.config.ts
@@ -1,8 +1,10 @@
 export const AppConfig = {
+  appName: process.env.APP_NAME,
   jwtSecret: process.env.SECRET,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
+  mongoUrl: process.env.CONNECTION_STRING,
 };
